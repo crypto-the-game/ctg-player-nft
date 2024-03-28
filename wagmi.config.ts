c0fc27bf806c9aab62d655228453e2cf109f04e9
Iain Nash <me@iain.in>
2024-03-28 01:38:33 -0400

    update wagmi config

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -43,7 +43,7 @@ const getAddresses = () => {
   };
 
   addAddress('DropMetadataRenderer', BASE_SEPOLIA_CHAIN_ID, '0xf643704CfB538aF53Dea5DFE9B1e795b9abf3bBf');
-  addAddress('CTGPlayerNFT', BASE_SEPOLIA_CHAIN_ID, '0xD3ebe4E02Fa46b54b60f695b8020a102734fe17C');
+  addAddress('CTGPlayerNFT', BASE_SEPOLIA_CHAIN_ID, '0x8e5f706ff23fdabd0c527577e4e0acfc98eac46f');
   addAddress('CTGPlayerNFTProxy', BASE_SEPOLIA_CHAIN_ID, '0xa80f737cC5BFdD1f1bD0e968F7deD6b624eA8935');
   addAddress('TransferPauserExtension', BASE_SEPOLIA_CHAIN_ID, '0xb62d3B471d55E9fBae2a6ec71F84f2D3af715f92');
 
